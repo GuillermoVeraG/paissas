c6221ee8235cbc6967fcb88005b31b5a00c448d1
Guillermo Vera Ghanain <guilleverag@gmail.com>
2024-03-18 18:18:39 -0500

    add hero component with unique slider not responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -51,6 +51,9 @@ export default {
       backgroundImage: {
         "menu-arrow": `url('/paissas/images/menu-arrow.webp')`,
         "menu-riped": "url('/paissas/images/menu-riped.png')",
+        "hero-slider-1": "url('/paissas/images/hero-slider-1-1.webp')",
+        "hero-slider-2": "url('/paissas/images/hero-slider-1-2.webp')",
+        "hero-shape": "url('/paissas/images/hero-shape.png')",
       },
     },
   },
